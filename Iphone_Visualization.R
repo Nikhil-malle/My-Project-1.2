64991cda750fe1955f09a1c6774df84a8f19e647
Nikhil Reddy <100486096+Nikhil-malle@users.noreply.github.com>
2022-05-13 12:31:35 +0530

    Update Iphone_Visualization.R

--- Iphone_Visualization.R
+++ Iphone_Visualization.R
@@ -5,6 +5,4 @@ library(ggplot2)
 
 ggplot(iphone_viz,aes(x = Name,fill = Int_Storage_GB)) + geom_bar()+facet_wrap(~Int_Storage_GB)
 
-ggplot(data = iphone_viz,aes(x = Name,y = Price,col = factor(Int_Storage_GB)))+geom_point(size = 3, color = 'purple')+geom_text(label = iphone_viz$Price,nudge_x = 0.09,nudge_y = 0)
-
 
