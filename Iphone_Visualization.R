3198f631fffe4b012b27fd3a0cd0f08fcb5f83fd
Nikhil Reddy <100486096+Nikhil-malle@users.noreply.github.com>
2022-05-17 10:22:28 +0530

    Update Iphone_Visualization.R

--- Iphone_Visualization.R
+++ Iphone_Visualization.R
@@ -3,13 +3,11 @@ View(iphone_viz)
 
 library(ggplot2)
 
-ggplot(iphone_viz,aes(x = Name,fill = Int_Storage_GB)) + geom_bar()+facet_wrap(~Int_Storage_GB)
-
-ggplot(iphone_viz,aes(x = Rating,y = Num_People_Rated,col = Name))+geom_point(size = 3)+geom_text(label = iphone_viz$Num_People_Rated,vjust = -1)
-
-ggplot(iphone_viz,aes(x = Name, y = Price))+geom_bar(stat = "identity")
+ggplot(iphone_viz,aes(y = Name,fill = Int_Storage_GB)) + geom_bar()+facet_wrap(~Int_Storage_GB)
 
 ggplot() +
   stat_identity(data = iphone_viz, aes(y = Color ,x =  Price,fill = factor(Int_Storage_GB)), geom = "bar")+ facet_grid(~Name)
 
+ggplot(iphone_viz,aes(x = Rating,y = Name))+geom_point(size = 3,col = "purple")+ 
+  scale_x_continuous(breaks = seq(0, 5, by = 0.1))+geom_text(label = iphone_viz$Rating,vjust = -1)
 
